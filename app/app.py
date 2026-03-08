d8193bc97a3c48718d192e6679b698f397503cf1
nedpearson <nedpearson@gmail.com>
2026-03-07 20:41:31 -0600

    Implement PWA Configuration to Preserve Microphone Permissions

--- app/app.py
+++ app/app.py
@@ -642,7 +642,16 @@ def universal_drilldown(type, id):
 
     return {"error": "Invalid drilldown type"}, 400
 
+from flask import send_from_directory
 import traceback
+
+@app.route('/manifest.json')
+def manifest():
+    return send_from_directory('static', 'manifest.json')
+
+@app.route('/service-worker.js')
+def service_worker():
+    return send_from_directory('static', 'service-worker.js', mimetype='application/javascript')
 from werkzeug.exceptions import HTTPException
 
 @app.errorhandler(Exception)
